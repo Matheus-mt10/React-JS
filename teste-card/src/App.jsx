694430c907429e273071a9cabba61a71241c9802
matheusDev <matheus.info29@gmail.com>
2023-11-18 08:21:22 -0300

    Att

---
+++ teste-card/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Card from './components/Card.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ teste-card/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import styles from './Card.module.css';
+
+function Card(params) {
+    return(
+        <div className="cardContainer">
+            
+        </div>
+    );
+}
+
+export default Card
