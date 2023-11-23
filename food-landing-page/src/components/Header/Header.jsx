e4732d3e77456c4f209782918c4783929c1b125b
matheusDev <matheus.info29@gmail.com>
2023-11-23 15:02:32 -0300

    Update

--- food-landing-page/src/components/Header/Header.jsx
+++ food-landing-page/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import style from './Header.module.css'
+import imgBrand from "../assets/continho.png"
 
 function Header () {
     return(
         <>
         <header>
             <div className="logoBrand">,
-                <img  className={style.brand} src="./assets/continho.png" alt=""/> 
+               <img className={style.brand} src={imgBrand} alt="logo" /> 
             </div>
         </header>
         </>
