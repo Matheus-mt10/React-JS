1f6cda7e68554221b53962a8dfb830eeb837459a
matheusDev <matheus.info29@gmail.com>
2023-11-27 13:11:15 -0300

    Update

--- food-landing-page/src/App.jsx
+++ food-landing-page/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
-// inspiração - https://   dribbble.com/shots/15248074-Food-Delivery-Landing-Page-UI-Design
+import AboutUs from './components/AboutUs/AboutUs';
+// inspiração - https://dribbble.com/shots/15248074-Food-Delivery-Landing-Page-UI-Design
 
 function App() {
   return (
     <div className="App">
     <Header></Header>
     <Hero></Hero>
+    <AboutUs></AboutUs>
     </div>
   );
 }
