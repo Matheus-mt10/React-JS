2234091962d0b3d50beaf4cea7a51f0a99e476f7
matheusDev <matheus.info29@gmail.com>
2023-11-28 23:47:15 -0300

    Update

--- props-card-test/src/App.jsx
+++ props-card-test/src/App.jsx
@@ -1,10 +1,32 @@
 import './App.css';
 import Card from './components/Card/Card';
+import airmax1 from './components/Assets/airmax1.png'
+import airmax2 from './components/Assets/airmax2.png'
+
 
 function App() {
   return (
     <div className="App">
-      <Card></Card>
+
+      <Card 
+      protoShoes={airmax1}
+      nameShoes={"Nike Air Max Tn Plus"}
+      DescriptionShoes={"O Air Max 90 é um dos principais sneakers da família Max Air. Desenvolvido pela Nike, foi lançada em 1990."}
+      priceShoes={"R$ 1000,00"}
+      >
+      
+      </Card>
+
+      <Card
+      protoShoes={airmax2}
+      nameShoes={"NOCTA Glide White"}
+      DescriptionShoes={"Apresentamos o NOCTA Glide. Inspirado no Zoom Flight 95s com o toque de NOCTA."}
+      priceShoes={"R$ 1200,00"}
+      
+      >
+      
+      </Card>
+
     </div>
   );
 }
--- props-card-test/src/components/Card/Card.jsx
+++ props-card-test/src/components/Card/Card.jsx
@@ -1,23 +1,22 @@
 import style from './Card.module.css';
-import airmar1 from '../Assets/airmax1.png';
+// photoShoes, nameShoes, DescriptionShoes, priceShoes
 
-function Card() {
+function Card({protoShoes, nameShoes, DescriptionShoes, priceShoes}) {
     
     return(
         <div className={style.Card}>
-            <img src={airmar1} className={style.airmax1} alt="Foto do airmax1" />
+            <img src={protoShoes} className={style.airmax1} alt="Foto do airmax1" />
 
             <div className={style.ContentContainer}>
-                <h2 className={style.cardTitle}>
-                Nike Air Max Tn Plus
+                <h2 className={style.cardTitle}> {nameShoes}
                 </h2>
 
                 <p className={style.cardDiscription}>
-                O Air Max 90 é um dos principais sneakers da família Max Air. Desenvolvido pela Nike, foi lançada em 1990.
+                {DescriptionShoes}
                 </p>
 
                 <div className={style.priceContent}>
-                <p className={style.price}><strong>R$ 1000,00</strong> 6x sem juros</p>
+                <p className={style.price}><strong>{priceShoes}</strong> 6x sem juros</p>
                 </div>
 
                 <div className={style.containerButton}><button className={style.ButtonAddCart}>Adicionar ao Carrinho</button></div>
