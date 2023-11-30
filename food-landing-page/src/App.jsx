1a0253d363d81c3d5741432da9e81e302875dc8c
matheusDev <matheus.info29@gmail.com>
2023-11-30 06:23:55 -0300

    Update

--- food-landing-page/src/App.jsx
+++ food-landing-page/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <Hero></Hero>
     <AboutUs></AboutUs>
     <Features></Features>
+    <AppDownload></AppDownload>
   
 
     </div>
--- food-landing-page/src/components/AppDownload/AppDownload.jsx
+++ food-landing-page/src/components/AppDownload/AppDownload.jsx
@@ -1,3 +1,7 @@
+import style from './AppDownload.module.css'
+import googleLabel from '../assets/googleLabel.png'
+import applelabel from '../assets/appleÇabel.png'
+
 function AppDownload(params) {
         return(
             <div className="appDownload">
@@ -6,12 +10,20 @@ function AppDownload(params) {
                         <h2 className="downloadTitle"> Download </h2>
                         <h2 className="downloadTitle2" >Get the app just now</h2>
                         <p className="downloadTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dignissimos quod cum eaque illum sunt, laborum tempore labore!</p>
+
+                    <div className="storesApps">
+
+                        <img className={style.brandsStore} src={googleLabel} alt="Google play label" />
+
+                        <img className={style.brandsStore} src={applelabel} alt="App store label" />
+                    
+
                     </div>
 
-                <div className="storesApps">
-                    
-                </div>
 
+                    </div>
+
+                
 
                 </div>
             </div>
