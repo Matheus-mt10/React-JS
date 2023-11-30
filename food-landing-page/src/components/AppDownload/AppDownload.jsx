bd38f0b15a938d7a8a33c9276c1eb4774d0d3ebd
matheusDev <matheus.info29@gmail.com>
2023-11-30 13:19:34 -0300

    Update

--- food-landing-page/src/components/AppDownload/AppDownload.jsx
+++ food-landing-page/src/components/AppDownload/AppDownload.jsx
@@ -4,28 +4,31 @@ import applelabel from '../assets/appleÇabel.png'
 
 function AppDownload(params) {
         return(
-            <div className="appDownload">
-                <div className="appBox">
-                    <div className="downloadTitleBox">
-                        <h2 className="downloadTitle"> Download </h2>
-                        <h2 className="downloadTitle2" >Get the app just now</h2>
-                        <p className="downloadTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dignissimos quod cum eaque illum sunt, laborum tempore labore!</p>
+            <div className={style.appDownload}>
+                <div className={style.appBox}>
 
-                    <div className="storesApps">
+                    <div className={style.downloadTitleBox}>
+                        <h2 className="downloadTitle"> Download </h2>
+                        <h2 className="downloadTitle2">Get the app just now</h2>
+                        <p className="downloadTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam dignissimos quod cum eaque illum sunt, laborum tempore labore!</p>
+                    </div>
+
+                    <div className={style.storesApps}>
 
                         <img className={style.brandsStore} src={googleLabel} alt="Google play label" />
 
                         <img className={style.brandsStore} src={applelabel} alt="App store label" />
                     
-
                     </div>
-
-
-                    </div>
-
-                
-
+                    
                 </div>
+
+
+                <div className="imgBox">
+                    
+                </div>
+
+
             </div>
         );
 }
