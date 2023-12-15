7293b43f654f8e435a48097f83d74b3c7f8b5e75
matheusDev <matheus.info29@gmail.com>
2023-12-15 06:55:35 -0300

    Update

--- sneaker-ui/src/components/Header/Header.jsx
+++ sneaker-ui/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import logo from '../assets/nikeBrand.png';
 import favorite from '../assets/favorite.png';
 import menu from '../assets/menu.png';
 import bag  from '../assets/bag.png';
+import search from '../assets/search.png';
 
 
 function Header() {
@@ -17,9 +18,13 @@ function Header() {
                     <a href="#"><img className={style.navIcons} src={favorite} alt="favorite icon" /></a>
                     <a href="#"><img className={style.navIcons} src={bag} alt="icon bag" /></a>
                     <a href="#"><img className={style.navIcons} src={menu} alt="Menu icon" /></a>
-
                 </div>
             </nav>
+
+            <div className={style.searchBar}>
+                <img  className={style.lupa} src={search} alt="Lupa" />
+                <input  className={style.inputSearchHome} placeholder='Buscar' type="search" name="" id="search"/>
+            </div>
 
             
 
