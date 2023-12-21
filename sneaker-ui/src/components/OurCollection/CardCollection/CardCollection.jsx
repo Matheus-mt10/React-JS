0e9fff5e61e61b4c200ddfaabe2d73c03f918e83
matheusDev <matheus.info29@gmail.com>
2023-12-21 00:37:47 -0300

    Update

--- sneaker-ui/src/components/OurCollection/CardCollection/CardCollection.jsx
+++ sneaker-ui/src/components/OurCollection/CardCollection/CardCollection.jsx
@@ -1,12 +1,13 @@
 import style from './dist/CardCollection.module.css';
 
-function CardCollection({imgSneaker, alt, sneakerTitle, sneakerPrice}) {
+function CardCollection({imgSneaker, alt, sneakerTitle, sneakerStyle,sneakerPrice}) {
     return(
 
         <div className={style.collectionCard}>
             <img className={style.imgSneaker} src={imgSneaker} alt={alt} />
             <div className={style.cardContent}>
                 <h2 className={style.sneakerTitle}>{sneakerTitle}</h2>
+                <p className={style.sneakerStyle}>{sneakerStyle}</p>
                 <p className={style.sneakerPrice}>{sneakerPrice}</p>
             </div>
         </div>
--- sneaker-ui/src/components/OurCollection/OurCollection.jsx
+++ sneaker-ui/src/components/OurCollection/OurCollection.jsx
@@ -6,13 +6,22 @@ function OurColletion(){
     return(
         <div className={style.collectionContainer}>
             <CardCollection
-            
             imgSneaker={nikeNocta}
             alt={"Sneaker nike nocta"}
-            sneakerTitle={"NOCTA x Nike Glide 'Black & White'"} 
+            sneakerTitle={"NOCTA x Nike Glide Black & White"} 
+            sneakerStyle={"Casual"}
             sneakerPrice={"150,00 R$"}
-            
             ></CardCollection>
+
+            <CardCollection
+            imgSneaker={nikeNocta}
+            alt={"Sneaker nike nocta"}
+            sneakerTitle={"NOCTA x Nike Glide Black & White"} 
+            sneakerStyle={"Casual"}
+            sneakerPrice={"150,00 R$"}
+            ></CardCollection>
+
+
         </div>
     );
 }
