bf379ca56aa1457f54e45887f0c406ec06c1e72d
matheusDev <matheus.info29@gmail.com>
2023-12-28 01:47:17 -0300

    Update

---
+++ sneaker-ui2/src/App.jsx
@@ -0,0 +1,15 @@
+// INSPIRATION: https://dribbble.com/shots/15735743-Ecommerce-Product-Page-Design
+
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      
+    </div>
+  );
+}
+
+export default App;
