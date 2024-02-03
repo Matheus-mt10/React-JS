ed90e26c99eec8044059db17f39c571691ca60ac
matheusDev <matheus.info29@gmail.com>
2024-02-02 23:51:05 -0300

    Update

--- login-ui/src/components/EmailInput/EmailInput.jsx
+++ login-ui/src/components/EmailInput/EmailInput.jsx
@@ -1,12 +1,16 @@
 import styles from './dist/EmailInput.module.css'
 import confirm_ico from '../assets/confirm_icon.png'
+import emailIco from '../assets/email.png'
 
 function EmailInput() {
     return(
         <div className={styles.emailContainer}>
-            <div className="emailIco">sasa</div>
-            <input type="email" name="" id="inputEmail" />
-            <div className="confirmIco"><img src={confirm_ico} alt="" /></div>
+            <img src={emailIco} alt="Email icon" className={styles.emailIco} />
+            <div className={styles.inputEmailBox}>
+                <p>Email Address</p>
+                <input type="email" name="" id={styles.inputEmail} />                
+            </div>
+            <div className={styles.confirmIco}><img src={confirm_ico} alt="" /></div>
         </div>
     );
 }
