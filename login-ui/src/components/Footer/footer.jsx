7862f0ab29088e5bac3dd78d29992f5ec1458141
matheusDev <matheus.info29@gmail.com>
2024-02-05 01:20:37 -0300

    Update

--- login-ui/src/components/Footer/footer.jsx
+++ login-ui/src/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ function footer() {
 
         <footer className={styles.footer}>
             
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis cum vitae! Doloribus dolorem, dicta est reiciendis repudiandae molestiae nemo, autem labore sed cum dolores optio. Autem voluptas fuga amet.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nobis cum vitae! Doloribus dolorem, dicta est reiciendis repudiandae molestiae nemo.</p>
            
         </footer>
 
