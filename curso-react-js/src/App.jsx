8b3091f21032947e94aded15dbd671d6847032c3
matheusDev <matheus.info29@gmail.com>
2024-02-13 20:25:12 -0300

    Update

--- curso-react-js/src/App.jsx
+++ curso-react-js/src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Evento01 from './components/eventos/Evento01';
 
 function App() {
   return (
     <div className="App">
+      <Evento01></Evento01>
      
     </div>
   );
---
+++ curso-react-js/src/components/eventos/Evento01.jsx
@@ -0,0 +1,11 @@
+function Evento01(params) {
+    return(
+        <div className="container">
+
+            <button className="btn"></button>
+
+        </div>
+    );
+}
+
+export default Evento01
